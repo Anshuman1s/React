e2868696c6ae7de6d9c63a714385a6ca0a9776e2
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-10 21:50:50 +0530

    Rmv

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Card from './Components/Card';
 import Cards from './Components/Cards';
 import Courses from './Components/Courses';
 function App(){
-    const data =["Anshuman","Ankur","Meethi","Kanaha"]
+    
   return (
     <div>
-      <Courses/>
+      {/* <Courses/> */}
        {/* <Cards/> */}
        {/* <Card/> */}
 
