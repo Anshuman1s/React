ae28e9b72562166b17cb3f0636b87926c3e20964
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-12 06:02:41 +0530

    UseState

--- src/Components/UseState.jsx
+++ src/Components/UseState.jsx
@@ -1,19 +1,30 @@
 import React, { useState } from 'react'
 
-function UseState() {
-    const [Score,SetScore] = useState(0)
-    const [banned,setBanned] = useState(false);
-    // const [name,ChangeName] = useState("Anshuman")
-    const [val,changeVal] = useState(12);
-    const [value,setValue] = useState({name:"Anshuman",isBanned:false})
-  return (
-    <div className='p-4 mt-3'>
-      {/* <h1>{name.toString()}</h1> */}
-      <h1>name is:{value.name}</h1>
-      <h2>isBanned {value.isBanned.toString()}</h2>
-    <button onClick={()=>setValue({...value, isBanned: !value.isBanned})} className='px-2 py-2 bg-blue-300 rounded-full '>Change</button>
-    </div>
-  )
-}
+// function UseState() {
+//     const [Score,SetScore] = useState(0)
+//     const [banned,setBanned] = useState(false);
+//     // const [name,ChangeName] = useState("Anshuman")
+//     const [val,changeVal] = useState(12);
+//     const [value,setValue] = useState({name:"Anshuman",isBanned:false})
+//   return (
+//     <div className='p-4 mt-3'>
+//       {/* <h1>{name.toString()}</h1> */}
+//       <h1>name is:{value.name}</h1>
+//       <h2>isBanned {value.isBanned.toString()}</h2>
+//     <button onClick={()=>setValue({...value, isBanned: !value.isBanned})} className='px-2 py-2 bg-blue-300 rounded-full '>Change</button>
+//     </div>
+//   )
+// }
+// function UseState(){
+//   const [check,changeVal] = ({name :"P", inLove:false})
+//   return (
+//     <div className='p-4 mt-3'>
+//       <h1>{check.name}</h1>
+//       <h2>in Love With Anshuman {check.inLove.toString()}</h2>
+//       <div className="px-2 py-2 bg-blue-300"></div>
+//       <button onClick={()=>changeVal({...check ,inLove : !check.inLove})} className='px-3 py-1 bg-blue-600'></button>
+//     </div>
+//   )
+// }
 
-export default UseState
+// export default UseState
