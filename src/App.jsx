d4bea6c27c5f1aec557157678fc46fc9c330f4e0
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-13 14:07:17 +0530

    Pr

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import React, { useState } from 'react';
-import Props from './Components/Props';
+// import React, { useState } from 'react';
+// import Props from './Components/Props';
 
-function App() {
-    const data = [
-        { name: "Alok", profession: "Gamer", image: "https://images.unsplash.com/photo-1531384441138-2736e62e0919?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend: false },
-        { name: "Ankur", profession: "Cricketer", image: "https://images.unsplash.com/photo-1493106819501-66d381c466f1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend: false },
-        { name: "Rahul", profession: "Singer", image: "https://images.unsplash.com/photo-1586716402203-79219bede43c?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend: false },
-        { name: "Mahesh", profession: "Coder", image: "https://images.unsplash.com/photo-1618393649689-c997c7455ef5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend: false },
-    ];
+// function App() {
+//     const data = [
+//         { name: "Alok", profession: "Gamer", image: "https://images.unsplash.com/photo-1531384441138-2736e62e0919?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend: false },
+//         { name: "Ankur", profession: "Cricketer", image: "https://images.unsplash.com/photo-1493106819501-66d381c466f1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend: false },
+//         { name: "Rahul", profession: "Singer", image: "https://images.unsplash.com/photo-1586716402203-79219bede43c?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend: false },
+//         { name: "Mahesh", profession: "Coder", image: "https://images.unsplash.com/photo-1618393649689-c997c7455ef5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend: false },
+//     ];
 
-    const [realdata, setRealdata] = useState(data);
+//     const [realdata, setRealdata] = useState(data);
 
-    // State jaha banti hai wahi modify bhi ki ja skti hai
-    const handleFriendsButton = (propindex) => {
-        setRealdata((previous) => {
-            return previous.map((item, index) => {
-                if (index === propindex) {
-                    return { ...item, friend: !item.friend };
-                }
-                return item;
-            });
-        });
-    };
+//     // State jaha banti hai wahi modify bhi ki ja skti hai
+//     const handleFriendsButton = (propindex) => {
+//         setRealdata((previous) => {
+//             return previous.map((item, index) => {
+//                 if (index === propindex) {
+//                     return { ...item, friend: !item.friend };
+//                 }
+//                 return item;
+//             });
+//         });
+//     };
 
-    return (
-        <div className="w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center">
-            {realdata.map((elem, index) => (
-                <Props
-                    key={index}
-                    image={elem.image}
-                    name={elem.name}
-                    profession={elem.profession}
-                    friend={elem.friend}
-                    handleClick={() => handleFriendsButton(index)}
-                />
-            ))}
-        </div>
-    );
-}
+//     return (
+//         <div className="w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center">
+//             {realdata.map((elem, index) => (
+//                 <Props
+//                     key={index}
+//                     image={elem.image}
+//                     name={elem.name}
+//                     profession={elem.profession}
+//                     friend={elem.friend}
+//                     handleClick={() => handleFriendsButton(index)}
+//                 />
+//             ))}
+//         </div>
+//     );
+// }
 
-export default App;
+// export default App;
 
 
 
@@ -101,3 +101,15 @@ export default App;
 //   )
 // }
 // export default App;
+
+import React from 'react'
+import Hello from './Components/Hello'
+function App() {
+  return (
+    <div>
+        <Hello/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/Hello.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function Hello() {
+    const [val,setVal] = useState(true)
+  return (
+    <div className='p-4'>
+        <button className={`px-3 py-3 ${val === true ? "bg-green-400" : "bg-blue-500"} absolute left-[45%] top-[20%] rounded text-xl font`} onClick={()=>{setVal(!val)}}>{val === true ? "Remove Friend" : "Add Friend"}</button>
+    </div>
+  )
+}
+
+export default Hello;
