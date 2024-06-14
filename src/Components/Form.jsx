a0f164bd9510603daa4ecfce7a08a37f26454c72
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-14 09:02:42 +0530

    Form Handling

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -43,5 +43,20 @@
 
 // export default Form
 
-// Using react hook form we can prevent the loading of thre form
+// Using react hook form we can prevent the loading of thee form
 
+import React from 'react'
+import {useForm} from 'react-hook-form'
+
+function Form() {
+    const {register,handleSubmit} = useForm();
+  return (
+    <form action="" onSubmit={handleSubmit(data=>console.log(data))}>
+        <input {...register('name')} type="text" placeholder='name' />
+        <input {...register('email')} type="email" placeholder='email' />
+        <input type="submit"/>
+    </form>
+  )
+}
+
+export default Form
